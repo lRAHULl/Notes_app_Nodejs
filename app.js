0696bd2ded1f0fd190157901f34dd9089360d1b5
lRAHULl <rahulpadmaraju16@gmail.com>
2018-11-30 13:15:01 +0530

    imported and exported my own files.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-console.log('Starting Notes app...')
+console.log('Starting App.JS');
 
 const fs = require('fs'); // File System Module.
 const os = require('os'); // Operating System.
+const notes = require('./notes.js');
 
-var user = os.userInfo()
+var user = os.userInfo();
 // console.log(user)
+
+var res = notes.addNote();
+console.log(res)
+
+var add = notes.add(2, -2);
+console.log(`The Sum is ${add}.`)
 
 fs.appendFile('test.txt', `Hello ${user.username}!`, (err) => {
     if (err) {
---
+++ notes.js
@@ -0,0 +1,13 @@
+console.log('Starting Notes.JS');
+
+
+// module.exports.age = 19
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'new Note'
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
