4aef91add519820c2112e777135679e87dcdd469
lRAHULl <rahulpadmaraju16@gmail.com>
2018-11-30 14:43:39 +0530

    Improved the app

--- app.js
+++ app.js
@@ -2,20 +2,38 @@ console.log('Starting App.JS');
 
 const fs = require('fs'); // File System Module.
 const os = require('os'); // Operating System.
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-var user = os.userInfo();
+const argv = yargs.argv;
+var command = process.argv[2]; // Used to get command line arguments.
+console.log(`Command: ${command}`);
+console.log('Yargs:',argv)
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.findNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not Recognized.');
+}
+
+// var user = os.userInfo();
 // console.log(user)
 
-var res = notes.addNote();
-console.log(res)
+// var res = notes.addNote();
+// console.log(res)
 
-var add = notes.add(2, -2);
-console.log(`The Sum is ${add}.`)
+// console.log(`The Sum is ${notes.add(10, -25)}.`)
 
-fs.appendFile('test.txt', `Hello ${user.username}!`, (err) => {
-    if (err) {
-        console.log('Some Problem ' + err);
-    }
-});
+// fs.appendFile('test.txt', `Hello ${user.username}!`, (err) => {
+//     if (err) {
+//         console.log('Some Problem ' + err);
+//     }
+// });
 
--- notes.js
+++ notes.js
@@ -1,13 +1,30 @@
 console.log('Starting Notes.JS');
 
-
 // module.exports.age = 19
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'new Note'
+var addNote = (title, body) => {
+    console.log('Adding Note: ', title, body);
 };
 
-module.exports.add = (a, b) => {
-    return a + b;
+var getAll = () => {
+    console.log('Getting All Notes.')
 };
+
+var findNote = (title) => {
+    console.log('Found the note with title:', title);
+}
+
+var removeNote = (title) => {
+    console.log('Removed the note with title:', title)
+}
+
+module.exports = {
+    addNote, // Actual Syntax:- addNote: addNote
+    getAll,
+    findNote,
+    removeNote
+}
+
+// module.exports.add = (a, b) => {
+//     return a + b;
+// };
