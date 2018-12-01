05beb268aa56089d254ed339d9042b6e4baf1ff0
lRAHULl <rahulpadmaraju16@gmail.com>
2018-12-01 13:57:01 +0530

    Added the note-add feature

--- notes.js
+++ notes.js
@@ -1,29 +1,44 @@
-console.log('Starting Notes.JS');
+const fs = require("fs");
 
-// module.exports.age = 19
+console.log("Starting Notes.JS");
 
 var addNote = (title, body) => {
-    console.log('Adding Note: ', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+  try {
+    var notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (err) {}
+
+  var duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
-    console.log('Getting All Notes.')
+  console.log("Getting All Notes.");
 };
 
-var findNote = (title) => {
-    console.log('Found the note with title:', title);
-}
+var findNote = title => {
+  console.log("Found the note with title:", title);
+};
 
-var removeNote = (title) => {
-    console.log('Removed the note with title:', title)
-}
+var removeNote = title => {
+  console.log("Removed the note with title:", title);
+};
 
 module.exports = {
-    addNote, // Actual Syntax:- addNote: addNote
-    getAll,
-    findNote,
-    removeNote
-}
+  addNote, // Actual Syntax:- addNote: addNote
+  getAll,
+  findNote,
+  removeNote
+};
 
 // module.exports.add = (a, b) => {
 //     return a + b;
